e3f8a04f693776a203acf1cb5289461f4b2654e1
Woodson Juste <j.woodson@hotmail.fr>
2022-01-10 17:32:22 +0100

    ajout formatage de chaine

--- baseJava/src/fr/dawan/syntaxe_de_base/entree_sortie_standard/FormatageDeChaineDeCaractere.java
+++ baseJava/src/fr/dawan/syntaxe_de_base/entree_sortie_standard/FormatageDeChaineDeCaractere.java
@@ -13,27 +13,27 @@ public class FormatageDeChaineDeCaractere {
 		int diviseur = 21;
 		
 		/*les sorties formatées: Le format de ce qui va être affiché
-		 *	%s	Permet de formater une chaine de caractères
-		 *	%c  Permet de formater Un caractère
-		 *  %d	Permet de formater un entier en base décimale.
-		 *  %x	Permet de formater un entier sous forme hexadécimale
-		 *  %e	Permet de formater un réel sous forme décimale en notation scientifique
-		 *  %f	Permet de formater un réel sous forme décimale
-		 *  %t	Utilisé pour formater les dates
+		 *	%s	Permet de formatter une chaine de caractères
+		 *	%c  Permet de formatter Un caractère
+		 *  %d	Permet de formatter un entier en base décimale.
+		 *  %x	Permet de formatter un entier sous forme hexadécimale
+		 *  %e	Permet de formatter un réel sous forme décimale en notation scientifique
+		 *  %f	Permet de formatter un réel sous forme décimale
+		 *  %t	Utilisé pour formatter les dates
 		 *  %n ou \n  (retour à la ligne)
 		 */
 		
-		//1ere solution : utilisation de la methode println
+		//1ere solution : utilisation de la méthode println
 		System.out.println("[" + dividende + "/" + diviseur + "]");
 		
 		//2ème solution : Utilisation de la méthode printf
 		System.out.printf("[%d/%d]\n", dividende, diviseur);
 		
-		//3ème solution : Utiisation de Sting.format
+		//3ème solution : Utilisation de Sting.format
 		String str = String.format("[%d/%d]", dividende, diviseur);
 		System.out.println(str);
 		
-		//Formater la valeur de PI sous forme decimal
+		//Formatter la valeur de PI sous forme decimal
 		System.out.printf("%f\n", Math.PI);
 		
 		//Arrondir la valeur de PI avec 3 chiffres après la virgule
